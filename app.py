51028f96a4e65404405a12da2884f19db8ac3a96
Zam <zamanwarkhan@gmail.com>
2022-09-11 19:35:21 +0530

    App changed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from telegram.ext.updater import Updater
 from telegram.ext.commandhandler import CommandHandler
-from telegram.ext.messagehandler import  MessageHandler
+from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 import os
 
@@ -25,19 +25,19 @@ def error(update, context):
     context.bot.send_message(update.message.chat.id, " Error !!")
 
 
-# def main():
-updater = Updater(token=TOKEN)
-dp = updater.dispatcher
-dp.add_handler(CommandHandler("start", start))
-dp.add_handler(CommandHandler("details", details))
-dp.add_handler(MessageHandler(Filters.text, mimic))
+def main():
+    updater = Updater(token=TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("details", details))
+    dp.add_handler(MessageHandler(Filters.text, mimic))
 
-dp.add_error_handler(error)
+    dp.add_error_handler(error)
 #    updater.start_webhook(listen="0.0.0.0", port=os.environ.get("PORT", 443),
 #                          url_path="https://zam-isbot.herokuapp.com/" + TOKEN)
-updater.start_polling()
-updater.idle()
+# updater.start_polling()
+    updater.idle()
 
 
-# if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
